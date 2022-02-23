9f2b1696f3e25f909dc16a5d3fc2248185173efd
liuxiaoliang <15810140695@163.com>
2022-02-23 18:24:39 +0800

    初始化

---
+++ cloud-nacos-config8801/src/main/java/com/ddcode/nacos/NaocsConfig8801.java
@@ -0,0 +1,11 @@
+package com.ddcode.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaocsConfig8801 {
+    public static void main(String[] args) {
+        SpringApplication.run(NaocsConfig8801.class);
+    }
+}
---
+++ cloud-nacos-config8801/src/main/java/com/ddcode/nacos/controller/NacosOrderController.java
@@ -0,0 +1,40 @@
+package com.ddcode.nacos.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.ddcode.nacos.config.OrderProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+@RestController
+public class NacosOrderController {
+
+    @Resource
+    private OrderProperties orderProperties;
+
+    @Value(value = "${order.pay-timeout-seconds}") // @NacosValue(value = "${order.pay-timeout-seconds}")
+    private Integer payTimeoutSeconds;
+    @Value(value = "${order.create-frequency-seconds}") // @NacosValue(value = "${order.create-frequency-seconds}")
+    private Integer createFrequencySeconds;
+
+    /**
+     * 测试 @ConfigurationProperties 注解的配置属性类
+     */
+    @GetMapping("/test01")
+    public OrderProperties test01() {
+        return orderProperties;
+    }
+
+    /**
+     * 测试 @Value 注解的属性
+     */
+    @GetMapping("/test02")
+    public Map<String, Object> test02() {
+        return new JSONObject().fluentPut("payTimeoutSeconds", payTimeoutSeconds)
+                .fluentPut("createFrequencySeconds", createFrequencySeconds);
+    }
+
+}
